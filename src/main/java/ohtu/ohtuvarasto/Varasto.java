f32e21405d2bc8b29b9ca8b17cc48c10d9667c30
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-03-19 22:41:51 +0200

    tahallinen checkstyle virhe

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -19,7 +19,8 @@ public class Varasto {
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
         this.setTilavuus(tilavuus);
-        if (alkuSaldo < 0.0) {
+        if (alkuSaldo < 0.0) 
+        {
             this.saldo = 0.0;
         } else if (alkuSaldo <= tilavuus) { // mahtuu
         
